484605094a5d811245ad533269aacc4bc79a18ba
lozo2010 <davidlglozo@gmail.com>
2016-10-02 11:39:58 +0200

    Add Statusbar tab

--- src/com/android/settings/Legend/LegendSettings.java
+++ src/com/android/settings/Legend/LegendSettings.java
@@ -31,10 +31,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import com.android.settings.Legend.tabs.GeneralUI;
-import com.android.settings.Legend.tabs.LockScreen;
-import com.android.settings.Legend.tabs.System;
-import com.android.settings.Legend.PagerSlidingTabStrip;
+import com.android.settings.nuclear.tabs.GeneralUI;
+import com.android.settings.nuclear.tabs.LockScreen;
+import com.android.settings.nuclear.tabs.StatusBar;
+import com.android.settings.nuclear.tabs.System;
+import com.android.settings.nuclear.PagerSlidingTabStrip;
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
@@ -156,10 +157,10 @@ public class LegendSettings extends SettingsPreferenceFragment {
             super(fm);
             frags[0] = new GeneralUI();
             frags[1] = new LockScreen();
-            /*frags[1] = new StatusBar();
-            frags[2] = new Navigation();
+            frags[2] = new StatusBar();
+            /*frags[2] = new Navigation();
             frags[3] = new Recents();*/
-            frags[2] = new System();
+            frags[3] = new System();
         }
 
         @Override
@@ -184,7 +185,7 @@ public class LegendSettings extends SettingsPreferenceFragment {
         titleString = new String[]{
                     getString(R.string.generalui_category),
                     getString(R.string.lockscreen_category),
-                    //getString(R.string.statusbar_category),
+                    getString(R.string.statusbar_category),
                     //getString(R.string.navigation_category),
                     //getString(R.string.multitasking_category),
                     getString(R.string.system_category)};
